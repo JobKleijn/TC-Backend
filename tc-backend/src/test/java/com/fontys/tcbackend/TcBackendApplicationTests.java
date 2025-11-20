513b7304f91722d8d3e4c5fb0b832b5d21e2e4eb
Job <jobkleijn@hotmail.com>
2025-11-20 15:47:43 +0100

    Added springboot project

---
+++ tc-backend/src/test/java/com/fontys/tcbackend/TcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fontys.tcbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
